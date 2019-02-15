8a57a289e6e628a1e3a1f13bb9141064d4a4d07b
ZeikkuSSJ7 <47304153+ZeikkuSSJ7@users.noreply.github.com>
2019-02-16 00:48:31 +0100

    Delete javaWin95.java

--- javaWin95.java
+++
@@ -1,6 +0,0 @@
-public class javaWin95{
-	public static void main (String[] args){
-		System.out.println("Hello world!");
-		System.out.println("Un programa de Java hecho con el notepad de Windows 95, lmao");
-	}
-}
